76a3e977264073f367348f78a46b1667afe0dc69
Aurélien <aurel.pp@free.fr>
2023-06-29 17:15:45 +0200

    feat: add swagger details

--- routes/products.js
+++ routes/products.js
@@ -6,73 +6,153 @@ const client = new MongoClient(url);
 const dbName = 'ZOO';
 const db = client.db(dbName);
 const collection = db.collection('data');
-
 const router = express.Router();
+
 router.use(express.json());
 
 /**
- * @openapi
- * /:
- *   get:
- *     description: Welcome to swagger-jsdoc!
+ * @swagger
+ * /create-data:
+ *   post:
+ *     description: Create a new data entry.
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/definitions/Data'
  *     responses:
  *       200:
- *         description: Returns a mysterious string.
+ *         description: Returns the result of the data creation.
+ *       400:
+ *         description: Invalid request body.
  */
 router.post('/create-data', async (req, res) => {
-    // Sending request to create a data
-    try {
-        const insertResult = await collection.insertOne({ text: req.body.text });
-        console.log (req.body.text, insertResult)
-        res.json(insertResult)
-      } catch (error) {
-        if (error instanceof MongoServerError) {
-          console.log(`Erreur : ${error}`);
-        }
-        throw error;
+  try {
+    const insertResult = await collection.insertOne({ text: req.body.text });
+    console.log(req.body.text, insertResult);
+    res.json(insertResult);
+  } catch (error) {
+    if (error instanceof MongoServerError) {
+      console.log(`Erreur : ${error}`);
     }
-})
+    res.status(400).json({ error: 'Invalid request body' });
+  }
+});
 
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     description: Get all data entries.
+ *     responses:
+ *       200:
+ *         description: Returns an array of data entries.
+ *       400:
+ *         description: Invalid request.
+ *       404:
+ *         description: No data entries found.
+ */
 router.get('/', async (req, res) => {
-    try {
-        const datas = await collection.find().toArray();
-        console.log (req.body.text, datas)
-        res.json(datas)
-      } catch (error) {
-        if (error instanceof MongoServerError) {
-          console.log(`Erreur : ${error}`); 
-        }
-        throw error;
+  try {
+    const datas = await collection.find().toArray();
+    console.log(req.body.text, datas);
+    if (datas.length > 0) {
+      res.json(datas);
+    } else {
+      res.status(404).json({ error: 'No data entries found' });
     }
-})
+  } catch (error) {
+    if (error instanceof MongoServerError) {
+      console.log(`Erreur : ${error}`);
+    }
+    res.status(400).json({ error: 'Invalid request' });
+  }
+});
 
+/**
+ * @swagger
+ * /update-data:
+ *   put:
+ *     description: Update a data entry by ID.
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/definitions/Data'
+ *     responses:
+ *       200:
+ *         description: Returns a success message.
+ *       400:
+ *         description: Invalid request.
+ *       404:
+ *         description: Data entry not found.
+ */
 router.put('/update-data', async (req, res) => {
-    // updating a data by it's ID and new value
-    db.collection('data').findOneAndUpdate(
-        { _id: new mongodb.ObjectId(req.body.id) },
-        { $set: { text: req.body.text } },
-        function () {
-        res.send('Success updated!')
-        }
-    )
-})
-
-router.delete('/delete-data', async (req, res) => {
-    // deleting a data by it's ID
+  const id = req.body.id;
+  const text = req.body.text;
+  if (!id || !text) {
+    res.status(400).json({ error: 'Invalid request body' });
+  } else {
     try {
-        const datas = await collection.deleteOne({ _id: req.body.id });
-        console.log(req, datas)
-        if (datas.deletedCount > 0) {
-            res.send('La donnée a bien été supprimé')
-        } else {
-            res.send('Aucune donnée n\'a été supprimé')
-        }
-      } catch (error) {
-        if (error instanceof MongoServerError) {
-          console.log(`Erreur : ${error}`);
-        }
-        throw error;
+      const updateResult = await collection.findOneAndUpdate(
+        { _id: ObjectId(id) },
+        { $set: { text } }
+      );
+      if (updateResult.value) {
+        res.send('Success updated!');
+      } else {
+        res.status(404).json({ error: 'Data entry not found' });
+      }
+    } catch (error) {
+      if (error instanceof MongoServerError) {
+        console.log(`Erreur : ${error}`);
+      }
+      res.status(400).json({ error: 'Invalid request' });
     }
-})
+  }
+});
+
+/**
+ * @swagger
+ * /delete-data:
+ *   delete:
+ *     description: Delete a data entry by ID.
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             properties:
+ *               id:
+ *                 type: string
+ *             example:
+ *               id: "6065d5e5afafab0d1c86ec1a"
+ *     responses:
+ *       200:
+ *         description: Returns a success or failure message.
+ *       400:
+ *         description: Invalid request.
+ *       404:
+ *         description: Data entry not found.
+ */
+router.delete('/delete-data', async (req, res) => {
+  const id = req.body.id;
+  if (!id) {
+    res.status(400).json({ error: 'Invalid request body' });
+  } else {
+    try {
+      const deleteResult = await collection.deleteOne({ _id: ObjectId(id) });
+      if (deleteResult.deletedCount > 0) {
+        res.send('La donnée a bien été supprimée');
+      } else {
+        res.status(404).json({ error: 'Data entry not found' });
+      }
+    } catch (error) {
+      if (error instanceof MongoServerError) {
+        console.log(`Erreur : ${error}`);
+      }
+      res.status(400).json({ error: 'Invalid request' });
+    }
+  }
+});
 
 export default router;
--- routes/sensitive-resource.js
+++
@@ -1,6 +0,0 @@
-import  express  from "express";
-
-const router = express.Router();
-import { isAuthentified } from "../middleware/is-authentified";
-
-router.get('/sensitive', isAuthentified,  sensitiveController.getData);
